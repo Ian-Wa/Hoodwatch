ccc80fd588810cd1b8506a86d920bcf6544ea5b2
Ian <ian.wanjira@student.moringaschool.com>
2022-01-12 16:22:39 +0300

    updated settings.py file

--- hood/settings.py
+++ hood/settings.py
@@ -51,7 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hoodWatch',
+    'hoods',
     'bootstrap4',
     'cloudinary',
     
@@ -148,10 +148,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
