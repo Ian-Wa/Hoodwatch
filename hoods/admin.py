ca757d581d2f0314e2d3b86ecb2f26b407f2ce66
Ian <ian.wanjira@student.moringaschool.com>
2022-01-12 16:13:34 +0300

    updated admin.py file in app folder

--- hoods/admin.py
+++ hoods/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from hoods.models import Business, Neighbourhood, Post, Profile
 
 # Register your models here.
+admin.site.register(Neighbourhood)
+admin.site.register(Profile)
+admin.site.register(Business)
+admin.site.register(Post)
